2f887b937907405a66f0edee4d3417521fafd141
Himanshu <pandehimanshu2@gmail.com>
2025-09-15 12:45:45 +0530

    After updation

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
         System.out.println("Hello World");
+        if (a > b) {
+            System.out.println("true");
+        }
     }
 }
